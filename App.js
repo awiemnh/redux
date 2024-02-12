4a5904f2bd52e00e2ea337ea853130569e81f50e
Arva Firjatullah <arvafg07@gmail.com>
2024-02-12 11:18:00 +0700

    bisa neeehhh

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { createStore } from "react-redux";
+import { createStore } from "redux";
 import notesReducer from "./src/reducers/notesReducer";
 import NoteForm from "./src/components/NoteForm";
 import NoteList from "./src/components/NoteList";
@@ -11,7 +11,14 @@ const store = createStore(notesReducer);
 const App = () => {
   return (
     <Provider store={store}>
-      <View>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 220,
+        }}
+      >
         <NoteForm />
         <NoteList />
       </View>
--- src/reducers/notesReducer.js
+++ src/reducers/notesReducer.js
@@ -1,12 +1,13 @@
 const initialState = {
-    notes: [],
+  notes: [],
 };
 const notesReducer = (state = initialState, action) => {
-    switch (action,type) {
-        case 'ADD_NOTE':
-            return {...state, notes: [...state,notes, action, payload]};
-            default:
-                return state;
-    }
+  switch (action.type) {
+    case "ADD_NOTE":
+      return { ...state, notes: [...state.notes, action.payload] };
+    default:
+      return state;
+  }
 };
+
 export default notesReducer;

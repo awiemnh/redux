b0c06b17734c94a090619e4477d0e5a200f7266c
yudharp <yudhaananda.rp@gmail.com>
2024-02-12 14:30:47 +0700

    dataReducer

---
+++ src/reducers/dataReducer.js
@@ -0,0 +1,17 @@
+// reducers/dataReducer.js
+const initialState = {
+    data: [],
+    error: null,
+  };
+  
+  export const dataReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case 'FETCH_DATA_SUCCESS':
+        return { ...state, data: action.payload, error: null };
+      case 'FETCH_DATA_ERROR':
+        return { ...state, error: action.payload };
+      default:
+        return state;
+    }
+  };
+  

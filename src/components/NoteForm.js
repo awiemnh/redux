9e39e8677640cc67d44191777c95312180c70567
Arva Firjatullah <arvafg07@gmail.com>
2024-02-12 11:45:09 +0700

    style nya broo

--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addNote } from "../actions/notesActions";
-import { View, TextInput, Button } from "react-native";
+import {
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+} from "react-native";
 
 const NoteForm = () => {
   const [noteText, setNoteText] = useState("");
@@ -17,13 +24,62 @@ const NoteForm = () => {
   return (
     <View>
       <TextInput
+        style={styles.input}
         placeholder="Type your note here"
         value={noteText}
         onChangeText={(text) => setNoteText(text)}
       />
-      <Button title="Add Note" onPress={handleAddNote} />
+      {/* <Button
+        style={styles.button}
+        title="Tambah Note"
+        onPress={handleAddNote}
+      /> */}
+      <View style={styles.bottonGroup}>
+        <TouchableOpacity style={styles.button} onPress={handleAddNote}>
+          <Text style={styles.buttonText}>Tambah Notes</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 220,
+  },
+  input: {
+    width: 250,
+    height: 40,
+    borderColor: "#EB455F",
+    borderWidth: 2,
+    paddingHorizontal: 10,
+    borderRadius: 24,
+    margin: 4,
+  },
+  button: {
+    backgroundColor: "#EB455F",
+    padding: 10,
+    margin: 10,
+    borderRadius: 24,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "500",
+    paddingHorizontal: 12,
+    textAlign: "center",
+  },
+  bottonGroup: {
+    padding: 10,
+    paddingVertical: 20,
+  },
+});
 
 export default NoteForm;
--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,21 +1,47 @@
 // components/NoteList.js
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { View, Text, FlatList } from 'react-native';
+import React from "react";
+import { useSelector } from "react-redux";
+import { View, Text, FlatList, StyleSheet } from "react-native";
 
 const NoteList = () => {
   const notes = useSelector((state) => state.notes);
 
   return (
     <View>
-      <Text>Notes:</Text>
+      <Text style={styles.judul}>Notes:</Text>
       <FlatList
         data={notes}
         keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item }) => <Text>{item}</Text>}
+        renderItem={({ item }) => <Text style={styles.fontlist}>{item}</Text>}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  judul: {
+    fontWeight: "700",
+    fontSize: 24,
+    textAlign: "center",
+    padding: 6,
+  },
+  list: {
+    borderWidth: 3,
+  },
+  fontlist: {
+    flex: 1,
+    flexDirection: "row",
+    color: "#EB455F",
+    fontWeight: "700",
+    fontSize: 18,
+    borderWidth: 2,
+    padding: 10,
+    margin: 4,
+    borderRadius: 8,
+    borderColor: "#EB455F",
+
+    textAlign: "center",
+  },
+});
 
 export default NoteList;
